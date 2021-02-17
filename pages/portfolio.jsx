2852f880303a9fce4b7f7c397de9ec146ee413b5
Ivan Urra <ivan.urra.wd@gmail.com>
2021-02-17 12:11:58 +0100

    projects styles

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -25,7 +25,11 @@ function Portfolio() {
 										<h4 className="portfolio-name">Cinemabox</h4>
 										<i className="fab fa-github"></i>
 										<i className="fas fa-external-link-alt"></i>
-										<div className="portfolio-category">Web App</div>
+										<div className="portfolio-category">
+											App about movies and TV shows. It allows you to keep up to date with 
+											the latest releases and the greatest hits of the moment. 
+											You can save your favorite movies and series so you don't miss anything.
+										</div>
 										<div>
 											<ul className="portfolio-stack">
 												<li>React</li>
@@ -41,8 +45,19 @@ function Portfolio() {
 										<h4 className="portfolio-name">Cocktail Guide</h4>
 										<i className="fab fa-github"></i>
 										<i className="fas fa-external-link-alt"></i>
-										<div className="portfolio-category">Web App</div>
-										<div className="portfolio-stack">Web App</div>
+										<div className="portfolio-category">
+											App about the world of cocktails. It contains a search engine that feeds on an API 
+											and a CRUD database in MongoDB Atlas with the users of the app and the recipes 
+											they can share with it.
+										</div>
+										<div>
+											<ul className="portfolio-stack">
+												<li>Handlebars</li>
+												<li>Express</li>
+												<li>Node</li>
+												<li>MongoDB</li>
+											</ul>
+										</div>
 									</div>
 									<div className="portfolio-item">
 										<img src="/politico2.png" className="portfolio-image" />
@@ -50,8 +65,19 @@ function Portfolio() {
 										<h4 className="portfolio-name">El Politico</h4>
 										<i className="fab fa-github"></i>
 										<i className="fas fa-external-link-alt"></i>
-										<div className="portfolio-category">Web Game</div>
-										<div className="portfolio-stack">Web App</div>
+										<div className="portfolio-category">
+											Videogame created with the challenge of finishing it in just four days. 
+											I used Canvas for the 2D graphics and it is developed with JavaScript, 
+											HTML & CSS. It is a tribute to the videogames that I played as a child.
+										</div>
+										<div>
+											<ul className="portfolio-stack">
+												<li>Canvas</li>
+												<li>JavaScript</li>
+												<li>HTML</li>
+												<li>CSS</li>
+											</ul>
+										</div>
 									</div>
 								</div>
 								{/* <a href="https://github.com/ivanurra" className="cta"><i className="fab fa-github gitlogo"></i>More Stuff</a> */}
