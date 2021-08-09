2b08100392dfedcab70215bf1b5ca935c082fd28
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-09 18:18:26 +0200

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,8 +18,14 @@ export default function Home() {
           property="og:description"
           content="Ivan Urra Portfolio - Full Stack Web Developer"
         />
-        <meta property="og:image" content="http://ivanurra.dev/mockup-pic.jpg/" />
-        <meta property="og:image:secure_url" content="https://ivanurra.dev/mockup-pic.jpg/" /> 
+        <meta
+          property="og:image"
+          content="http://ivanurra.dev/mockup-pic.jpg/"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://ivanurra.dev/mockup-pic.jpg/"
+        />
         <meta
           property="og:site_name"
           content="Ivan Urra Full Stack Web Developer"
@@ -62,7 +68,18 @@ export default function Home() {
             </h1>
             <div className="col-xl-7 pl-0">
               <p className="myName-description">
-                I'm a Full Stack Web Developer specializing in building (and occasionally designing) exceptional digital experiences.
+                I'm a Full Stack Web Developer specializing in building (and
+                occasionally designing) exceptional digital experiences.
+                Currently, I'm focused on building usable, memorable and
+                exclusive digital products at{" "}
+                <a
+                  href="https://secuoyas.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="color-company">Secuoyas</span>
+                </a>
+                .
               </p>
             </div>
             <a href="mailto:ivan.urra.dev@gmail.com" className="cta">
