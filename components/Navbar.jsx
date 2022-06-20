a86b50ad888ee8b5050656f761c2cbcee0e8b44c
Ivan Urra <ivan.urra.wd@gmail.com>
2022-06-20 07:22:25 +0200

    WIP

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,48 +1,12 @@
-import React from "react";
 import { Link } from "react-scroll";
 import { Navbar, Nav } from "react-bootstrap";
-import { motion } from "framer-motion";
 
 export default function Navigation() {
-  // const onToggleConfetti = () => {
-  //   confetti({
-  //     zIndex: 999,
-  //     particleCount: 200,
-  //     spread: 160,
-  //     angle: -90,
-  //     origin: {
-  //       x: 1,
-  //       y: 0,
-  //     },
-  //   });
-  // };
-
   return (
     <Navbar expand="lg" variant="dark" className="fixed-top color-nav">
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            y: 30,
-            scale: 1,
-            opacity: 0,
-          },
-          visible: {
-            y: 0,
-            scale: 1,
-            opacity: 1,
-            transition: {
-              duration: 0.6,
-              delay: 0,
-            },
-          },
-        }}
-      >
-        <Navbar.Brand href="/" className="navbar-brand">
-          ivanurra<span className="color-number">.dev</span>
-        </Navbar.Brand>
-      </motion.div>
+      <Navbar.Brand href="/" className="navbar-brand">
+        ivanurra<span className="color-number">.dev</span>
+      </Navbar.Brand>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -54,28 +18,9 @@ export default function Navigation() {
             smooth={true}
             duration={1300}
           >
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 1,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    duration: 1,
-                    delay: 0,
-                  },
-                },
-              }}
-            >
-              <a href="/about">
-                <span className="color-number">01.</span>About
-              </a>
-            </motion.div>
+            <a href="/about">
+              <span className="color-number">01.</span>About
+            </a>
           </Link>
 
           <Link
@@ -85,28 +30,9 @@ export default function Navigation() {
             smooth={true}
             duration={1500}
           >
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 1,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    duration: 1.5,
-                    delay: 0,
-                  },
-                },
-              }}
-            >
-              <a href="/portfolio">
-                <span className="color-number">02.</span>Portfolio
-              </a>
-            </motion.div>
+            <a href="/portfolio">
+              <span className="color-number">02.</span>Portfolio
+            </a>
           </Link>
 
           <Link
@@ -116,58 +42,15 @@ export default function Navigation() {
             smooth={true}
             duration={1900}
           >
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 1,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    duration: 2,
-                    delay: 0,
-                  },
-                },
-              }}
-            >
-              <a href="/contact">
-                <span className="color-number">03.</span>Contact
-              </a>
-            </motion.div>
+            <a href="/contact">
+              <span className="color-number">03.</span>Contact
+            </a>
           </Link>
 
           <a>
-            <motion.div
-              initial="hidden"
-              animate="visible"
-              variants={{
-                hidden: {
-                  scale: 1,
-                  opacity: 0,
-                },
-                visible: {
-                  scale: 1,
-                  opacity: 1,
-                  transition: {
-                    duration: 2.5,
-                    delay: 0,
-                  },
-                },
-              }}
-            >
-              <a
-                href="/resumeivanurra.pdf"
-                className="resume"
-                download
-                // onClick={onToggleConfetti}
-              >
-                Resume
-              </a>
-            </motion.div>
+            <a href="/resumeivanurra.pdf" className="resume">
+              Resume
+            </a>
           </a>
         </Nav>
       </Navbar.Collapse>
