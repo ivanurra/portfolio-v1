89cbd8043e45d5a839109c49166df68e7f8461d4
Ivan Urra <ivan.urra.wd@gmail.com>
2023-05-08 18:21:25 +0200

    WIP

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -21,13 +21,22 @@ export const Skills = () => {
         <i className="fas fa-caret-right"></i> Express JS
       </li>
       <li>
-        <i className="fas fa-caret-right"></i> MongoDB
-      </li>
-      <li>
         <i className="fas fa-caret-right"></i> HTML
       </li>
       <li>
         <i className="fas fa-caret-right"></i> (S)CSS
+      </li>
+      <li>
+        <i className="fas fa-caret-right"></i> MongoDB
+      </li>
+      <li>
+        <i className="fas fa-caret-right"></i> PostgreSQL
+      </li>
+      <li>
+        <i className="fas fa-caret-right"></i> Docker
+      </li>
+      <li>
+        <i className="fas fa-caret-right"></i> Kubernetes
       </li>
     </div>
   );
